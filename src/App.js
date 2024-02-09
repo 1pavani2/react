445be6d4a0f319a8f764e3a93eee20ed60d1a639
Pavani Ambati <ambatipavani2002@gmail.com>
2024-02-09 12:51:37 +0530

    Revert "react:completed the dialpad task"
    This reverts commit 026f4a9f670da47cfbfbc31c7813f1738dcb889e.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Page from './page';
-import Dialpad from './dialpad';
-import { useState,useEffect } from 'react';
-import Calculator from './calculator';
-function App() {
-  // var x=10
-  // const [a, setA] = useState(10)
-  // console.log('PAVANI..a', x,a)
-  const[pavani,ambati]=useState('hello')
-  // const handletoclick=()=>{
-  //   ambati("madhuri")
-  // }
- 
-  return (
-    // <div className="App">
 
-    <div>
-      {/* <p>{pavani}</p> */}
-      {/* <Page></Page> */}
-      {/* <Dialpad></Dialpad> */}
-      <Calculator></Calculator>
-      {/* {3==2?'Hi':'Heelo'}
-      pavani */}
-      {/* {a}
-      <button onClick={() => { x = x+10; console.log('x=', x, "a=", a); setA(a+10) }} title='HEllo' className='h-10 w-10 bg-[red] text-[blue]' /> */}
-    {/* <button onClick={()=>{
-      ambati("madhuri")}}>clickme!</button> */}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
